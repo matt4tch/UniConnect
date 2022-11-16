c8a96e4a144571a6f987bc71b2a06340d6ae60ff
Matthew Tchouikine <107216405+matt4tch@users.noreply.github.com>
2022-11-15 19:58:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,53 @@
 import googlemaps
+from googlemaps import convert
 from datetime import datetime
 
+# CONSTANT DEFINITIONS
+locations = [{
+                "lat" : 43.472107,
+                "lng" : -80.543938
+             }, {
+                "lat" : 43.471593,
+                "lng" : -80.550003
+             }, {
+                "lat" : 43.469592,
+                "lng" : -80.542266
+            }, {
+                "lat" : 43.467925,
+                "lng" : -80.541740
+            }, {
+                "lat" : 43.472329,
+                "lng" : -80.542001
+            }]
+names = ['V1', 'MC', 'DP', 'HH', 'DC Library']
+
+# Retrieves a Google Maps Object
 gmaps = googlemaps.Client(key='AIzaSyDD4V7yHsGuEztB3sRbcQFfjAZYyZhBHm4')
 
-# Geocoding an address
-geocode_result = gmaps.geocode('Village 1, Waterloo ON')
-# Look up an address with reverse geocoding
-reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
-
-# Request directions via public transit
+# Request directions via walking
 now = datetime.now()
-directions_result = gmaps.directions("Ron Eydt Village",
-                                     "Waterloo, ON",
-                                     mode="walking",
-                                     departure_time=now)
-print(directions_result[0]['legs'][0]['distance'])
+
+
+directions_result = gmaps.directions({
+                "lat" : 43.472107,
+                "lng" : -80.543938
+             }, {
+                "lat" : 43.472329,
+                "lng" : -80.542001
+            }, mode="walking", departure_time=now)
+
+# Path Finder Algorithm:
+# Creates a list with all possible combinations of two locations (list of two dictionaries)
+# Each list is passed into gmaps.directions() to retrieve data
+#
+
+distance = directions_result[0]['legs'][0]['distance']['value']
+distance = distance / 1000
+print(f'{distance} km')
+duration = directions_result[0]['legs'][0]['duration']['value']
+minutes = duration // 60
+seconds = duration % 60
+print(f'{minutes} minutes and {seconds} seconds')
+print(directions_result[0]['legs'][0]['end_address'])
+print(directions_result[0]['legs'][0]['start_address'])
+
