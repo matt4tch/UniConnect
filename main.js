d515e39d77c42048804f73dbe37751b7ee3701ca
richardsha19 <113735719+richardsha19@users.noreply.github.com>
2022-11-20 11:02:34 -0500

    Added Directions to html file

--- main.js
+++ main.js
@@ -1,27 +1,64 @@
-import googlemaps
-from datetime import datetime
-
-from googlemaps import convert
-
-gmaps = googlemaps.Client(key='AIzaSyDD4V7yHsGuEztB3sRbcQFfjAZYyZhBHm4')
-
-from datetime import datetime
-# Request directions via public transit
-now = datetime.now()
-directions_result = gmaps.directions({
-                   "lat" : 43.472107,
-                   "lng" : -80.543938
-                }, {
-                   "lat" : 43.471593,
-                   "lng" : -80.550003
-                }, mode="walking", departure_time=now)
-
-distance = directions_result[0]['legs'][0]['distance']['value']
-distance = distance / 1000
-print(f'{distance} km')
-duration = directions_result[0]['legs'][0]['duration']['value']
-minutes = duration // 60
-seconds = duration % 60
-print(f'{minutes} minutes and {seconds} seconds')
-print(directions_result[0]['legs'][0]['end_address'])
-print(directions_result[0]['legs'][0]['start_address'])
+function initMap() {
+   const map = new google.maps.Map(document.getElementById("map"), {
+     zoom: 4,
+     center: { lat: 43.4643, lng: 80.5204 }, 
+   });
+   const directionsService = new google.maps.DirectionsService();
+   const directionsRenderer = new google.maps.DirectionsRenderer({
+     draggable: true,
+     map,
+     panel: document.getElementById("panel"),
+   });
+ 
+   directionsRenderer.addListener("directions_changed", () => {
+     const directions = directionsRenderer.getDirections();
+ 
+     if (directions) {
+       computeTotalDistance(directions);
+     }
+   });
+   displayRoute(
+     "Toronto, ON",
+     "Waterloo, ON",
+     directionsService,
+     directionsRenderer
+   );
+ }
+ 
+ function displayRoute(origin, destination, service, display) {
+   service
+     .route({
+       origin: origin,
+       destination: destination,
+       waypoints: [
+         { location: "Cambridge, ON" },
+         { location: "Guelph, ON" },
+       ],
+       travelMode: google.maps.TravelMode.WALKING,
+       //avoidTolls: true,
+     })
+     .then((result) => {
+       display.setDirections(result);
+     })
+     .catch((e) => {
+       alert("Could not display directions due to: " + e);
+     });
+ }
+ 
+ function computeTotalDistance(result) {
+   let total = 0;
+   const myroute = result.routes[0];
+ 
+   if (!myroute) {
+     return;
+   }
+ 
+   for (let i = 0; i < myroute.legs.length; i++) {
+     total += myroute.legs[i].distance.value;
+   }
+ 
+   total = total / 1000;
+   document.getElementById("total").innerHTML = total + " km";
+ }
+ 
+ window.initMap = initMap;
