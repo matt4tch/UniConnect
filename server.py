55e6ff83c37f93fcb1852fb5facda06bb13df554
richardsha19 <113735719+richardsha19@users.noreply.github.com>
2022-11-21 17:05:56 -0500

    Created server.py

---
+++ server.py
@@ -0,0 +1,25 @@
+from flask import *
+import json, time
+
+app = Flask (__name__)
+
+@app.route('/', methods=['GET'])
+def home_page():
+    data_set = {'Message': 'Succsesfully loaded the Home page', 'Timestamp': time.time()}
+    json_dump = json.dumps(data_set)
+
+    return json_dump
+
+@app.route('/', methods=['GET'])
+def request_page():
+    user_query = str(request.args.get('user')) #/user/?user=User_Name
+
+    data_set = {'Message': f'Succsesfully got the request for {user_query}', 'Timestamp': time.time()}
+    json_dump = json.dumps(data_set)
+
+    return json_dump
+
+if __name__ == '__main__':
+    app.run(port=3541)
+
+
