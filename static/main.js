2ee3113c0c4952424e103039a22bdec7975e80a3
richardsha19 <113735719+richardsha19@users.noreply.github.com>
2022-12-27 11:54:33 -0500

    Created a server connection

--- static/main.js
+++ static/main.js
@@ -1,3 +1,5 @@
+
+
 function initMap() {
    const map = new google.maps.Map(document.getElementById("map"), {
      zoom: 4,
---
+++ static/server.js
@@ -0,0 +1,11 @@
+const server_URL = "https://dd84-99-228-112-134.ngrok.io/";
+console.log("Hit");
+async function getUserInfo() {
+    const promise = await fetch(server_URL);
+    const processedReponse = await promise.json();
+    console.log("Reached");
+    alert(processedReponse);
+}
+
+
+document.querySelector('.Determine_Results').addEventListener("click", getUserInfo);
