ecede59f290807576d929b8765b60f821e875da5
richardsha19 <113735719+richardsha19@users.noreply.github.com>
2023-01-05 21:35:16 -0500

    Removed Key

--- React/src/fetchGeoLocation.js
+++ React/src/fetchGeoLocation.js
@@ -1,6 +1,6 @@
 async function fetchGeoLocation({queryKey}){
     const location = queryKey[1];
-    const res = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${location}}&key=AIzaSyDD4V7yHsGuEztB3sRbcQFfjAZYyZhBHm4`);
+    const res = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${location}}&key=`GOOGLE_API_KEY`);
     if(!res.ok)
         throw new Error(`An error occured while fetching: ${location}`);
     
