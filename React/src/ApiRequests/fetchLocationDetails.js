9d243d7723cb8d9c4d30033b1cf8fc8013f28575
richardsha19 <113735719+richardsha19@users.noreply.github.com>
2023-01-11 12:53:48 -0500

    Updated Server

--- React/src/ApiRequests/fetchLocationDetails.js
+++ React/src/ApiRequests/fetchLocationDetails.js
@@ -1,6 +1,6 @@
 async function fetchLocationDetails(){
     const message = "key";
-    const res = await fetch(`https://6e51-99-228-112-134.ngrok.io/location_info/`,{
+    const res = await fetch(`https://798b-129-97-124-19.ngrok.io/location_info/`,{
         method: "POST",
         headers: { "Content-Type": "application/json"},
         body: JSON.stringify(message)
